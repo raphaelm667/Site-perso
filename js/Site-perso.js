b6125efd0b27221d5b9836d0bae5750d746c20ba
raphaelm667 <71373096+raphaelm667@users.noreply.github.com>
2020-12-13 21:46:55 +0100

    Add files via upload

--- js/Site-perso.js
+++ js/Site-perso.js
@@ -4,12 +4,60 @@ let compteurMenu = 1;
 let menus = [];
 let i= 0;
 
+/**
+  * Initialise un tableau et le transmet a notre tableau générale + remplis le tableau html
+  *
+  * @param {Object} le formulaire recupéré
+  * @return {Boolean} return false car la page n'est pas liée a un server
+  */
+
 function ajouterMenu(x)
 {
-	let menu = ["N°"+compteurMenu,x.clientNom.value, x.clientPrenom.value, x.DateCommande.value, x.sandwich.value,x.boisson.value];
+	let temp="";
+	let menu = {
+		Numero :compteurMenu,
+		Nom: x.clientNom.value,
+		Prenom: x.clientPrenom.value,
+		Date: x.DateCommande.value,
+		Sandwich: x.sandwich.value,
+		Boisson: x.boisson.value
+		};
 	compteurMenu++;
 	menus[i] = menu;
+	menus.sort(ordoner);
 	i++;
-	document.getElementById("menuAffichage").innerHTML += "<p>Le Menu "+menu[0]+" a bien été ajoutée avec le "+menu[4]+".</p>";
+	for(let y=0;y<menus.length;y++){
+	temp += "<tr><td>N°"+menus[y]["Numero"]+"</td><td>"+menus[y]["Nom"]+"</td><td>"+menus[y]["Date"].toLocaleString('fr-BE')+"</td><td><a href='img/jambon.jpg' target='_blank'>"+menus[y]["Sandwich"]+"</a></td><td>"+menus[y]["Boisson"]+"</td>";
+	temp += "<td><input id='calc' name='champCalc' type='button' value='Calcul' onclick='calcul(0);'> </td>"
+	temp += "<td>"+calcul(y)+"</td></tr>";
+	document.getElementById("remplir").innerHTML = temp;
+	}
 	return false;
+}
+
+/**
+  *  Initialise le tableau au chargement de la page
+  *
+  * @action modifie le code html
+  */
+
+function  initialiserPage(){
+	document.getElementById("menuAffichage").innerHTML += "<table><thead><th>Numéro</th><th>Nom</th><th>Date</th><th>Sandwich</th><th>Boisson</th><th>Calcul</th><th>Prix</th></thead><tbody id='remplir'></tbody></table>";
+}
+
+function calcul(x)
+{
+	let prix=2;
+	if(menus[x]["Boisson"] == "Coca"){
+		prix+=3;
+	}
+	else if(menus[x]["Boisson"] == "Sprite"){prix+=2;}
+	else if(menus[x]["Boisson"] == "Fanta"){prix+=2;}
+	else{prix+=1;}
+	return prix+"€"
+}
+
+function ordoner(x,y)
+{
+	return y.Numero-x.Numero;
 }
